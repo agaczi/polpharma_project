eb702d160ba02b65ef40f86fadf00dac13f2b4b9
Agata Maciejewska <aamaciejewska@gmail.com>
2016-02-24 13:42:21 +0100

    changes in header section --> bootstrap

--- js/app.js
+++ js/app.js
@@ -1,3 +1,35 @@
 /**
  * Created by Agata on 19.02.2016.
  */
+$(document).ready(function() {
+
+
+// -------- // HAMBURGER MENU // -------- //
+
+    function setMenu() {
+        if ($(window).width() < 915) {
+            $('.hamburger').hide();
+            $('.menuBtn').show();
+        } else {
+            $('.hamburger').show();
+            $('.menuBtn').hide();
+            $('.mobileMenu').hide();
+        }
+    }
+
+    function setClick() {
+        $('.menuBtn').on('click', function () {
+            $('.mobileMenu').toggle();
+        });
+    }
+
+
+    setMenu();
+
+    $(window).on('resize', function () {
+        setMenu();
+    });
+    setClick();
+
+
+});
